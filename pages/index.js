731d74d05d6c014f290bea8cb15e8a80a9dffdaf
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-11-22 19:06:46 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,37 @@ export default function Home() {
     <>
       <Head>
         <title>Sahib Singh&apos;s Links</title>
-        <meta name="description" content="Sahib Singh's Links" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="Sahib Singh's Links" />
+        <meta name="author" content="Sahib Singh" />
+        <meta
+          name="description"
+          content="Sahib Singh's Links"
+        />
+        <meta
+          name="keywords"
+          content="Sahib Singh, Sahib Singh's Links, Sahib Singh's Linktree, Sahib, Soorme, Sardar Sahib Singh, Sahib Singh Khanna, Sahib Singh Github, Sahib Singh LinkedIn, Sahib Singh Gaming Geeks, ssahibsingh, Gaming Geeks, Soorme.com, Giigaa.com "
+        />
+        <link rel="canonical" href="https://ssahibsingh.github.io/links" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ssahibsingh.github.io/links" />
+        <meta property="og:title" content="Sahib Singh" />
+        <meta
+          property="og:description"
+          content="Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ"
+        />
+        <meta property="og:image" content="./website.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://ssahibsingh.github.io/links" />
+        <meta property="twitter:title" content="Sahib Singh" />
+        <meta
+          property="twitter:description"
+          content="Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ"
+        />
+        <meta property="twitter:image" content="./website.png" />
       </Head>
 
-      <nav className="navbar navbar-expand-lg my-4">
+      <nav className="navbar navbar-expand-lg my-3">
         <div className="content container d-flex justify-content-between">
           <div className="d-flex justify-content-center align-items-center">
             <Image src={logo} className="logo img-fluid mx-2" alt="Logo" />
@@ -26,7 +52,7 @@ export default function Home() {
         </div>
       </nav>
 
-      <main className="my-5">
+      <main className="mt-4">
         <div className="content container">
           <div className="row d-flex justify-content-center">
             {links.map((item) => {
