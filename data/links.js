b52c27d9369a33dd0e26b5286f9aeeca82ec2296
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-11-22 18:49:18 +0530

    added all necessary things

---
+++ data/links.js
@@ -0,0 +1,46 @@
+const links = [
+    {
+        id: 1,
+        name: "Website",
+        about: "Look at my work!",
+        url: "https://ssahibsingh.github.io",
+        img: require('../public/assets/website.png')
+    },
+    {
+        id: 2,
+        name: "Github",
+        about: "@ssahibsingh | 🏡 to all my projects",
+        url: "https://github.com/ssahibsingh",
+        img: require('../public/assets/github.png')
+    },
+    {
+        id: 3,
+        name: "LinkedIn",
+        about: "@ssahibsingh | Know more about my profession",
+        url: "https://www.linkedin.com/in/ssahibsingh/",
+        img: require('../public/assets/linkedin.png')
+    },
+    {
+        id: 4,
+        name: "Twitter",
+        about: "@ssahibsingh_ | 😜",
+        url: "https://twitter.com/ssahibsingh_",
+        img: require('../public/assets/twitter.png')
+    },
+    {
+        id: 5,
+        name: "Instagram",
+        about: "@ssahibsingh_ | Shots of my Life",
+        url: "https://www.instagram.com/ssahibsingh_/",
+        img: require('../public/assets/instagram.png')
+    },
+    {
+        id: 6,
+        name: "Telegram",
+        about: "@cexnone | Chat with me instantly",
+        url: "https://t.me/cexnone",
+        img: require('../public/assets/telegram.png')
+    },
+];
+
+export default links;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        crossorigin="anonymous"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
